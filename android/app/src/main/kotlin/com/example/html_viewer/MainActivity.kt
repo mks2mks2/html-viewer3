78576a9d8afa8c43ada6570f3f8c4b4817bec04e
mks2mks2 <maiconcosilva@gmail.com>
2026-03-20 09:49:13 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/html_viewer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/html_viewer/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.example.html_viewer
 
+import android.webkit.WebView
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onStart() {
+        super.onStart()
+        WebView.setWebContentsDebuggingEnabled(false)
+        val webView = WebView(this)
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+    }
+}
